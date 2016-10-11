3f3c2ea4bb1e03983f1811803980c5a5ae31b4d9
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2016-10-11 16:08:51 +0200

    Puhs #11 : Infos & point corrections
    Created information tab, corrected incorrect latlng points for districts

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,45 @@
 
 $("#analyse").slideUp(1);
 $("#infos").slideUp(1);
+$("#results").slideUp(1);
 
 $("#analyseLink").click(function(){
     $("#infos").slideUp();
+	$("#results").slideUp();
+	
     $("#analyse").slideDown();
+	
     declareEvents();
 });
 
 $("#infosLink").click(function(){
     $("#analyse").slideUp();
+	$("#results").slideUp();
+	
     $("#infos").slideDown();
+	
+    declareEvents();
+});
+
+$("#resultsLink").click(function(){
+    $("#analyse").slideUp();
+	$("#infos").slideUp();
+	
+    $("#results").slideDown();
+	
     declareEvents();
 });
 
 $("#mapLink").click(function(){
     $("#analyse").slideUp();
     $("#infos").slideUp();
+	$("#results").slideUp();
 });
 
 function declareEvents(){
     $(".close").unbind('click').click(function(){
         $("#analyse").slideUp();
         $("#infos").slideUp();
+		$("#results").slideUp();
     });
 }
